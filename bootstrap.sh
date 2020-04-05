59b0bb50af982e25feb9ce6393b8485e9f3eefb6
peterHoburg <peterHoburg@users.noreply.github.com>
2020-04-05 12:06:37 -0600

    GH-1: Moved from using apt/brew to install fzf to just git cloning it

--- bootstrap.sh
+++ bootstrap.sh
@@ -191,6 +191,7 @@ function main() {
 
   git clone https://github.com/zsh-users/zsh-autosuggestions ${ZSH_CUSTOM:-~/.oh-my-zsh/custom}/plugins/zsh-autosuggestions
   git clone https://github.com/zsh-users/zsh-syntax-highlighting.git ${ZSH_CUSTOM:-~/.oh-my-zsh/custom}/plugins/zsh-syntax-highlighting
+  git clone --depth 1 https://github.com/junegunn/fzf.git ~/.fzf; ~/.fzf/install
 
   source ~/.zshrc
 
